df7927e3fe3d956d36d9f6c42435685a7e718f90
Carlos Peña <carlos.07888@gmail.com>
2020-08-02 16:15:58 -0400

    Off-canvas menu: JavaScript functionality

--- js/index.js
+++ js/index.js
@@ -6,10 +6,25 @@ closeSearchButton.onclick = closeSearchBox;
 
 var searchArea = document.getElementById("searchArea");
 
+var headerMenuButton = document.getElementById("headerMenuButton");
+headerMenuButton.onclick = openOffCanvasMenu;
+
 function openSearchBox() {
 	searchArea.classList.add("active");
 }
 
 function closeSearchBox() {
 	searchArea.classList.remove("active");
+}
+
+function openOffCanvasMenu() {
+	if (document.body.classList.contains("menu-active"))
+	{
+		document.body.classList.remove("menu-active");
+	}
+	else
+	{
+		document.body.classList.add("menu-active");
+	}
+	
 }
